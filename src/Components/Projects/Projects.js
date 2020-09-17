4f01352079251157824d751cd5d0fed70b06ecbd
Murad Zalov <mzalov@gmail.com>
2020-09-16 23:31:13 -0500

    Finished up initial draft of projects page

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -8,6 +8,13 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import GeoWeather1 from '../../Static/Carousel/GeoWeather1.png'
 import GeoWeather2 from '../../Static/Carousel/GeoWeather2.png'
+import CrossSnap1 from '../../Static/Carousel/CrossSnap1.png'
+import CrossSnap2 from '../../Static/Carousel/CrossSnap2.png'
+import CrossSnap3 from '../../Static/Carousel/CrossSnap3.png'
+import CrossSnap4 from '../../Static/Carousel/CrossSnap4.png'
+import DouglasFir1 from '../../Static/Carousel/DouglasFir1.png'
+import DouglasFir2 from '../../Static/Carousel/DouglasFir2.png'
+import DouglasFir3 from '../../Static/Carousel/DouglasFir3.png'
 
 
 export default function Projects() {
@@ -42,7 +49,7 @@ export default function Projects() {
                   Description:
                 </div>
                 <p className='description-content'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  GeoWeather is a web application I developed that fetches temperature, weather conditions, and city image based on a user's input. The frontend is built using modular React components and styled with CSS. I used React Hooks in this application to learn first-hand about the benefits of foregoing 'this' context and method binding. This application is deployed using Netlify.
                 </p>
               </div>
               <div className='technologies-header-and-content'>
@@ -109,7 +116,7 @@ export default function Projects() {
                   Description:
                 </div>
                 <p className='description-content'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  The International Space Station Tracker is a web application I developed for fun to plot the location of the ISS while in orbit onto a world map. This project is a work-in-progress, and I aim to also add a feature that allows users to pinpoint a coordinate set on the world map and receive a set of times that the ISS will be visible in the night sky for them. The frontend is built using modular React components with CSS styling, and this application is powered by a number of API such as React-Maps and OpenNotify. This application is deployed using AWS Amplify.
                 </p>
               </div>
               <div className='technologies-header-and-content'>
@@ -117,12 +124,12 @@ export default function Projects() {
                   Technologies Used:
                 </div>
                 <p className='technologies-content'>
-                  React, React Maps, React Hooks, AWS, HTML, CSS, JavaScript, RESTFUL API
+                  React, React Maps, AWS, HTML, CSS, JavaScript, RESTful API
                 </p>
               </div>
             </div>
 
-            {/* ********* CAROUSEL ********* */}
+            {/* ********* CAROUSEL *********
             <div className='carousel-div'>
               <Carousel className='carousel-container' {...getConfigurableProps()}>
                 <div>
@@ -132,18 +139,18 @@ export default function Projects() {
                   <img src={GeoWeather2} alt='' />
                 </div>
               </Carousel>
-            </div>
+            </div> */}
 
             {/* ********* LINKS ********* */}
             <div className='links-div'>
               <div className='github-link-div'>
-                <a href="https://github.com/muradzalov/weatherApp2020" rel="noopener noreferrer" target="_blank">
+                <a href="https://github.com/muradzalov/SpaceStationTracker/tree/master/src" rel="noopener noreferrer" target="_blank">
                   <img src={GitHubLogo} alt='' className='link-icon' /> GitHub
                 </a>
               </div>
 
               <div className='deployed-link-div'>
-                <a href="https://weather-app-mz-2020.netlify.app/" rel="noopener noreferrer" target="_blank">
+                <a href="https://master.d11vj5ny5dd1v.amplifyapp.com/" rel="noopener noreferrer" target="_blank">
                   <img src={DeployedSiteIcon} alt='' className='link-icon' /> Deployed Site
                 </a>
               </div>
@@ -176,7 +183,7 @@ export default function Projects() {
                   Description:
                 </div>
                 <p className='description-content'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  I created a personal website to practice using basic HTML, CSS, and modularized React components. This site as a hub for my projects and contact information and is deployed through AWS Amplify and Route 53.
                 </p>
               </div>
               <div className='technologies-header-and-content'>
@@ -184,7 +191,7 @@ export default function Projects() {
                   Technologies Used:
                 </div>
                 <p className='technologies-content'>
-                  React, HTML, CSS
+                  React, HTML, CSS, AWS Amplify, AWS Route 53
                 </p>
               </div>
             </div>
@@ -192,13 +199,13 @@ export default function Projects() {
             {/* ********* LINKS ********* */}
             <div className='links-div'>
               <div className='github-link-div'>
-                <a href="https://github.com/muradzalov/weatherApp2020" rel="noopener noreferrer" target="_blank">
+                <a href="https://github.com/muradzalov/personal-website" rel="noopener noreferrer" target="_blank">
                   <img src={GitHubLogo} alt='' className='link-icon' /> GitHub
                 </a>
               </div>
 
               <div className='deployed-link-div'>
-                <a href="https://weather-app-mz-2020.netlify.app/" rel="noopener noreferrer" target="_blank">
+                <a href="https://www.muradzalov.com/" rel="noopener noreferrer" target="_blank">
                   <img src={DeployedSiteIcon} alt='' className='link-icon' /> Deployed Site
                 </a>
               </div>
@@ -231,7 +238,7 @@ export default function Projects() {
                   Description:
                 </div>
                 <p className='description-content'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  CrossSnap is a multiplayer crossword mobile application that allows users to complete New York Times crossword puzzles together. The application features real-time gameplay with multiple users, game and user persistence, and a dynamic custom-built crossword screen. I built this project with my team as part of Fullstack Academy's senior phase. My contributions to this project include the architecture of back-end schemas for the crossword JSON repository, navigation inside the mobile application, conditional rendering of screens fetching data from the database, and construction of the crossword game board.
                 </p>
               </div>
               <div className='technologies-header-and-content'>
@@ -248,10 +255,16 @@ export default function Projects() {
             <div className='carousel-div'>
               <Carousel className='carousel-container' {...getConfigurableProps()}>
                 <div>
-                  <img src={GeoWeather1} alt='' />
+                  <img src={CrossSnap1} alt='' />
                 </div>
                 <div>
-                  <img src={GeoWeather2} alt='' />
+                  <img src={CrossSnap2} alt='' />
+                </div>
+                <div>
+                  <img src={CrossSnap3} alt='' />
+                </div>
+                <div>
+                  <img src={CrossSnap4} alt='' />
                 </div>
               </Carousel>
             </div>
@@ -259,14 +272,8 @@ export default function Projects() {
             {/* ********* LINKS ********* */}
             <div className='links-div'>
               <div className='github-link-div'>
-                <a href="https://github.com/muradzalov/weatherApp2020" rel="noopener noreferrer" target="_blank">
+                <a href="https://github.com/CrossCollab/CrossSnap" rel="noopener noreferrer" target="_blank">
                   <img src={GitHubLogo} alt='' className='link-icon' /> GitHub
-                </a>
-              </div>
-
-              <div className='deployed-link-div'>
-                <a href="https://weather-app-mz-2020.netlify.app/" rel="noopener noreferrer" target="_blank">
-                  <img src={DeployedSiteIcon} alt='' className='link-icon' /> Deployed Site
                 </a>
               </div>
             </div>
@@ -298,7 +305,7 @@ export default function Projects() {
                   Description:
                 </div>
                 <p className='description-content'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  Douglas Fir Apothecary is an e-commerce web application that welcomes users to browse and shop for a variety of apothecary products featuring Stripe integrated checkout, persistent guest/authenticated user experiences, and Google open authorization. I developed this project with my team as part of Fullstack's senior phase. My contributions include the creation of the conditional view of all products on the homepage, protection of the back-end routes that allow users to commit their selected items and quantities to the database, and the incorporation of Google OAuth for a persistent and seamless login experience.
                 </p>
               </div>
               <div className='technologies-header-and-content'>
@@ -315,10 +322,13 @@ export default function Projects() {
             <div className='carousel-div'>
               <Carousel className='carousel-container' {...getConfigurableProps()}>
                 <div>
-                  <img src={GeoWeather1} alt='' />
+                  <img src={DouglasFir1} alt='' />
                 </div>
                 <div>
-                  <img src={GeoWeather2} alt='' />
+                  <img src={DouglasFir2} alt='' />
+                </div>
+                <div>
+                  <img src={DouglasFir3} alt='' />
                 </div>
               </Carousel>
             </div>
@@ -326,13 +336,13 @@ export default function Projects() {
             {/* ********* LINKS ********* */}
             <div className='links-div'>
               <div className='github-link-div'>
-                <a href="https://github.com/muradzalov/weatherApp2020" rel="noopener noreferrer" target="_blank">
+                <a href="https://github.com/1911-Grace-Shopper-Team-Douglas-Fir/GraceShopper" rel="noopener noreferrer" target="_blank">
                   <img src={GitHubLogo} alt='' className='link-icon' /> GitHub
                 </a>
               </div>
 
               <div className='deployed-link-div'>
-                <a href="https://weather-app-mz-2020.netlify.app/" rel="noopener noreferrer" target="_blank">
+                <a href="https://douglasfirapothecary.herokuapp.com/" rel="noopener noreferrer" target="_blank">
                   <img src={DeployedSiteIcon} alt='' className='link-icon' /> Deployed Site
                 </a>
               </div>
